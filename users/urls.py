ef2f7fe1de1b810d2d859ddc594e8f3581f77a33
aleksandr <sanek_ves@mail.ru>
2023-09-12 19:23:51 +0300

    Добавлена возможность смены пароля

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import PasswordChangeView
-from django.urls import path
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
 from users.views import UserLoginView, UserLogoutView, UserRegisterView, UserUpdateView, activate_new_user
@@ -12,5 +12,6 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),  # регистрация пользователя
     path('profile/', UserUpdateView.as_view(), name='profile'),  # изменения данных пользователя
     path('activate/<int:pk>/', activate_new_user, name='activate'),  # активация пользователя
-    path('password_change/', PasswordChangeView.as_view(template_name='users/login.html'), name='change_password')  # смена пароля пользователя
+    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html', success_url=reverse_lazy('users:password_change_done')), name='change_password'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
